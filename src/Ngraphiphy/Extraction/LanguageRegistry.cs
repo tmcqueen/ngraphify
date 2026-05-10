85e4c67727ccb7ca61ccf58ebc9b783a0a0587e3
Tim McQueen <tim.mcqueen@gmail.com>
2026-05-10 08:18:04 -0500

    feat: add ILanguageExtractor interface and registry

---
+++ src/Ngraphiphy/Extraction/LanguageRegistry.cs
@@ -0,0 +1,29 @@
+namespace Ngraphiphy.Extraction;
+
+public sealed class LanguageRegistry
+{
+    private readonly Dictionary<string, ILanguageExtractor> _byExtension = new(StringComparer.OrdinalIgnoreCase);
+    private readonly List<ILanguageExtractor> _extractors = [];
+
+    public void Register(ILanguageExtractor extractor)
+    {
+        _extractors.Add(extractor);
+        foreach (var ext in extractor.SupportedExtensions)
+            _byExtension[ext] = extractor;
+    }
+
+    public ILanguageExtractor? GetExtractor(string filePath)
+    {
+        var ext = Path.GetExtension(filePath).ToLowerInvariant();
+        return _byExtension.GetValueOrDefault(ext);
+    }
+
+    public IReadOnlyList<ILanguageExtractor> All => _extractors;
+
+    public static LanguageRegistry CreateDefault()
+    {
+        var registry = new LanguageRegistry();
+        // Extractors will be registered here as they are implemented
+        return registry;
+    }
+}
